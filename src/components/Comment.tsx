fef9d6c9db8342652be33b23b7e26812ae106f24
guimonteirojbv <guilhermemonteiroro@gmail.com>
2023-06-06 09:43:55 -0300

    commit inicial

---
+++ src/components/Comment.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = React.useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => state + 1);
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/guimonteirojbv.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Guilherme Monteiro</strong>
+              <time title="11 de maio às 8:13" dateTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
